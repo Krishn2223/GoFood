3223498d9598aed2a50d8703b522d69068dd2886
Krishn2223 <krishn.p@ahduni.edu.in>
2024-06-02 23:37:57 +0530

    modified CORS

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
 const mongoDB = require("./db");
 
-
 // giving permisson (have to write it)
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
-///////////////////
+const corsOptions = {
+  origin: "http://localhost:3000", // Adjust this to match your frontend origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+};
 
+app.use(cors(corsOptions));
+///////////////////
 
 mongoDB();
 app.use(express.json());
 
-app.use('/api', require("./Routes/CreateUser"));
-app.use('/api', require("./Routes/DisplayData"));
-app.use('/api', require("./Routes/OrderData"));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 import Home from "./Screens/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
@@ -14,23 +10,19 @@ import Login from "./Screens/Login.js";
 import { CartProvider } from "./components/ContextReducer.js";
 import MyOrders from "./Screens/MyOrders.js";
 
-
-
-
-
 function App() {
   return (
     <CartProvider>
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/createuser" element={<Signup/>} />
-          <Route exact path="/myOrder" element={<MyOrders/>} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/createuser" element={<Signup />} />
+            <Route exact path="/myOrder" element={<MyOrders />} />
+          </Routes>
+        </div>
+      </Router>
     </CartProvider>
   );
 }
