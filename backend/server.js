d5a58d0d9885260042f297bbb2367d5b209fdf0d
Krishn2223 <krishn.p@ahduni.edu.in>
2024-08-21 19:59:19 +0530

    removed index.js

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,26 @@ const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 
-// giving permisson (have to write it)
+// CORS options
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
-    // "http://go-food-frontend-seven.vercel.app",
-  ], // Adjust this to match your frontend origin
+    // "http://localhost:3000",
+    "http://go-food-frontend-seven.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
 };
 
 app.use(cors(corsOptions));
-///////////////////
 
 mongoDB();
 app.use(express.json());
 
-app.use("/api", require("./Routes/CreateUser"));
-app.use("/api", require("./Routes/DisplayData"));
-app.use("/api", require("./Routes/OrderData"));
+// Use routes without '/api' prefix
+app.use(require("./Routes/CreateUser"));
+app.use(require("./Routes/DisplayData"));
+app.use(require("./Routes/OrderData"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
